19b3afdd6526e7da735a75e4856f68054a7213eb
erik78se <erik.lonroth@gmail.com>
2022-02-05 16:18:21 +0100

    Initial pieces for letsencrypt

--- operator-nextcloud/src/interface_http.py
+++ operator-nextcloud/src/interface_http.py
@@ -4,7 +4,8 @@
 from ops.framework import Object
 from ops.charm import RelationBrokenEvent, RelationDepartedEvent
 import logging
-
+from jinja2 import Environment
+import yaml
 
 class HttpProvider(Object):
     """
@@ -19,6 +20,22 @@ class HttpProvider(Object):
         self._relation_name = relation_name
         self._hostname = hostname  # FQDN of host passed on in relations
         self._port = port
+        # The services key to pass to haproxy
+        self._haproxyServicesYaml = """
+- service_name: nextcloud
+  service_host: 0.0.0.0
+  service_port: 443
+  crts: [DEFAULT]
+  service_options:
+      - balance leastconn
+      - option forwardfor
+      - http-request set-header X-Forwarded-Port %[dst_port]
+      - http-request add-header X-Forwarded-Proto https if { ssl_fc }
+      - http-check expect status 200
+      - acl url_discovery path /.well-known/caldav /.well-known/carddav
+      - http-request redirect location /remote.php/dav/ code 301 if url_discovery
+  servers: [[nextcloud_unit_{{ unitid }}, {{ address }}, {{ port }}, 'cookie S{i} check']]
+"""
         self.framework.observe(
             charm.on[relation_name].relation_joined, self._on_relation_joined
         )
@@ -49,9 +66,24 @@ class HttpProvider(Object):
             logging.debug("Nextcloud installed, sending relation data to remote haproxy.")
             event.relation.data[self.model.unit]['hostname'] = self._hostname
             event.relation.data[self.model.unit]['port'] = str(self._port)
+            event.relation.data[self.model.unit]['services'] = self._renderServicesYaml()
 
     def _on_relation_departed(self, event: RelationDepartedEvent):
         pass
 
     def _on_relation_broken(self, event: RelationBrokenEvent) -> None:
         pass
+
+    def _renderServicesYaml(self):
+        YAML = self._haproxyServicesYaml
+        
+        ip = str(self.model.get_binding("website").network.bind_address)
+
+        r = Environment().from_string(YAML).render(address=ip,
+                                                   port=self._port,
+                                                   unitid=self.model.unit.name.rsplit('/', 1)[1])
+
+        try:
+            return str(yaml.safe_load(r))
+        except yaml.YAMLError as exc:
+            print(exc)
