efacb65b15d2f8247c6a6ebb04401a0249a65d51
erik78se <erik.lonroth@gmail.com>
2022-02-07 14:23:46 +0100

    more

--- operator-nextcloud/tests/__init__.py
+++ operator-nextcloud/tests/__init__.py
@@ -4,3 +4,5 @@ path = os.path.dirname(__file__)
 path = os.path.join(path, 'src')
 if path not in sys.path:
     sys.path.append(path)
+
+print("======================== > SYS.PATH" + str(sys.path))
