5a169e90da4a42e18180e03c85fc9415c1763740
erik78se <erik.lonroth@gmail.com>
2022-02-08 03:00:44 +0100

    now config based haproxy relation

--- operator-nextcloud/src/interface_http.py
+++ operator-nextcloud/src/interface_http.py
@@ -21,22 +21,9 @@ class HttpProvider(Object):
         self._relation_name = relation_name
         self._hostname = hostname  # FQDN of host passed on in relations
         self._port = port
+        self._haproxy_service_name = "nextcloud"
         # The services key to pass to haproxy
-        self._haproxyServicesYaml = """
-- service_name: nextcloud
-  service_host: 0.0.0.0
-  service_port: 443
-  crts: [DEFAULT]
-  service_options:
-      - balance leastconn
-      - option forwardfor
-      - http-request set-header X-Forwarded-Port %[dst_port]
-      - http-request add-header X-Forwarded-Proto https if { ssl_fc }
-      - http-check expect status 200
-      - acl url_discovery path /.well-known/caldav /.well-known/carddav
-      - http-request redirect location /remote.php/dav/ code 301 if url_discovery
-  servers: [[nextcloud_unit_{{ unitid }}, {{ address }}, {{ port }}, 'cookie S{i} check']]
-"""
+
         self.framework.observe(
             charm.on[relation_name].relation_joined, self._on_relation_joined
         )
@@ -52,11 +39,14 @@ class HttpProvider(Object):
 
     def _on_relation_joined(self, event):
         """
-        We use this event for passing on hostname and port.
-        :param event:
-        :return:
+        We would normally be able to send information here, but we don't want to
+        give the haproxy this information until we are really
         """
-        logging.debug("Joining with haproxy.")
+        haproxy_ip = event.relation.data[event.unit]['private-address']
+        logging.debug("Joining with haproxy: " + haproxy_ip )
+
+        # TODO: We can add this to trustred proxies here
+
 
     def _on_relation_changed(self, event):
         if not self.charm._is_nextcloud_installed():
@@ -64,10 +54,11 @@ class HttpProvider(Object):
             event.defer()
             return
         else:
-            logging.debug("Nextcloud installed, sending relation data to remote haproxy.")
+            haproxy_ip = event.relation.data[event.unit]['private-address']
+            logging.debug("Nextcloud installed, sending relation data to remote haproxy at." + str(haproxy_ip))
             event.relation.data[self.model.unit]['hostname'] = self._hostname
             event.relation.data[self.model.unit]['port'] = str(self._port)
-            event.relation.data[self.model.unit]['services'] = self._renderServicesYaml()
+            event.relation.data[self.model.unit]['service_name'] = "nextcloud"
 
     def _on_relation_departed(self, event: RelationDepartedEvent):
         pass
@@ -75,6 +66,7 @@ class HttpProvider(Object):
     def _on_relation_broken(self, event: RelationBrokenEvent) -> None:
         pass
 
+    # TODO: Possibly dead code here since we dont send anything to haproxy this way now.
     def _renderServicesYaml(self):
         YAML = self._haproxyServicesYaml
         ip = str(self.model.get_binding("website").network.bind_address)
