d69f4b0c0b11ca53ad62b900c21a8bd824cda8b2
Joakim Nyman <joakim.nyman86@gmail.com>
2022-02-16 23:52:44 +0100

    added backup port and user config parameters

--- operator-nextcloud/scripts/backup/run_backup.sh
+++ operator-nextcloud/scripts/backup/run_backup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #
-# Create ssh-keys for root first and move over public key to BACKUPHOST
+# Create ssh-keys for root first and move over public key to backup_host
 #
 set -x
 
 echo "Running backup" | wall
 
-BACKUPHOST={{ backup_host }}
-
 /root/scripts/backup/Nextcloud-Backup-Restore/NextcloudBackup.sh > /root/backuplog.log 2>&1
 
-rsync -v --remove-source-files -Aax /backups/* nextcloud@${BACKUPHOST}: >> /root/backuplog.log 2>&1
+rsync -v --remove-source-files -Aax -e "ssh -p {{ backup_port }}" /backups/* {{ backup_user }}@{{ backup_host }}: >> /root/backuplog.log 2>&1
 
 if grep --quiet -i ERROR /root/backuplog.log;
 then
